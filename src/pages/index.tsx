5782ab75d10e058bdf0da402c1e0e58386f9b8d5
LuizMeraki <luizollvrsantos@gmail.com>
2022-12-23 17:22:19 -0300

    Sending files

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>SushiScript</title>
+      </Head>
+      <main>
+
+      </main>
+    </>
+  );
+}
+
+export default Home;
