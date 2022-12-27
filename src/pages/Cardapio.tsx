13a686be20856192b1cbb508f9b8afa58ff5e15c
LuizMeraki <luizollvrsantos@gmail.com>
2022-12-27 19:02:26 -0300

    Pages: new page has been added

---
+++ src/pages/Cardapio.tsx
@@ -0,0 +1,21 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+
+const Cardapio = () => {
+  return (
+    <>
+
+      <Header />
+
+      <main>
+        <h1>Hello world</h1>
+      </main>
+
+      <Footer />
+
+    </>
+  );
+}
+
+export default Cardapio;
