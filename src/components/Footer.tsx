608b180586a647af07991dad600d5502b7bf5653
LuizMeraki <luizollvrsantos@gmail.com>
2022-12-29 11:13:50 -0300

    Update: new background color has been added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="container-padding bg-[#e6d6d0] mt-28 py-4">
+    <footer className="container-padding bg-[#e6e6e6] mt-28 py-4">
       <div className="max-width">
         <p className="text-center">&copy; 2022 - SushiScript</p>
       </div>
